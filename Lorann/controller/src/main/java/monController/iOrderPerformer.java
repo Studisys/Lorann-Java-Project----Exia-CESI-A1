a7b415f365f7b7bfc52946b81a81e52587014eaf
SweepzFr <thomas.lourenco@viacesi.fr>
2018-05-29 16:43:08 +0200

    Correction fautes

--- Lorann/controller/src/main/java/monController/iOrderPerformer.java
+++ Lorann/controller/src/main/java/monController/iOrderPerformer.java
@@ -3,5 +3,5 @@ package monController;
 //An interface called iOrderPerformer
 public interface iOrderPerformer 
 {
-	public void orderPerform(keyOrder keyOrder);
+	public void orderPerform(KeyOrder keyOrder);
 }
