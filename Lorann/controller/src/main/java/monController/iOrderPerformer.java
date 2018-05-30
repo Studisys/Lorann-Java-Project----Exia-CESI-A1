02b87b019b50e89c3348f9c99e99793eb31d621a
Ragnay <32931624+Ragnay@users.noreply.github.com>
2018-05-30 17:36:24 +0200

    bisous

--- Lorann/controller/src/main/java/monController/iOrderPerformer.java
+++ Lorann/controller/src/main/java/monController/iOrderPerformer.java
@@ -3,5 +3,5 @@ package monController;
 //An interface called iOrderPerformer
 public interface iOrderPerformer 
 {
-	public void orderPerform(iKeyOrder keyOrder);
+	public void orderPerform(KeyOrder keyOrder);
 }
