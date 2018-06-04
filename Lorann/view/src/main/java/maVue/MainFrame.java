e3f24c64bf01f6308ba370e8e6024d8672fceb28
Studisys <joel.didier@viacesi.fr>
2018-06-04 11:10:02 +0200

    Updating (hope it works)

--- Lorann/view/src/main/java/maVue/MainFrame.java
+++ Lorann/view/src/main/java/maVue/MainFrame.java
@@ -29,46 +29,12 @@ public class MainFrame implements java.lang.Runnable,iView {
 		SwingUtilities.invokeLater(this);
 	}
 	
+	
 	public void run()
-	{
-		JFrame frame = new JFrame("Lorann");
-		frame.setTitle("Lorann");
-		String userLevelInput = JOptionPane.showInputDialog(frame, "Please enter the level number you wish to be loaded :", "Lorann - Enter the desired level number", JOptionPane.PLAIN_MESSAGE);
-						
-		// If users closes Input Window, kill the process.
-		if (userLevelInput.equals(null)) {
-			System.out.println("User closed the input window ! Aborting...");
-			System.exit(0);
-		}
-		
-		// Cast user input (String) to Integer
-		int userIntInput = Integer.parseInt(userLevelInput); 
-		System.out.println("User chose the level number : " + userIntInput);
-		
-		// Instantiate LorannDAO to run getLevelMapById
-		LorannDAO dao = new LorannDAO();
-		
-		// New object of type Example
-		Example daoReturn = null;
-		
-		// Store answer from MySQL server to daoReturn object
-		try
-		{
-			daoReturn=LorannDAO.getLevelMapById(userIntInput);
-			System.out.println("Map String Answer from MySQL server : " + daoReturn);
-		}
-		catch (SQLException e)
-		{
-			e.printStackTrace();
-		}
-		
-		
-		// Assign Answer from Server to level string
-		this.level = String.valueOf(daoReturn);
-		
+	{	
 		this.gameFrame = new GameFrame("Lorann", this.eventPerformer, this.graphicsBuilder, observable);
-
 	}
+	
 	public void displayMessage(String message)
 	{
 		JOptionPane.showMessageDialog(null, message);
